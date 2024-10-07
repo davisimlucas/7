d9cc3e7126f1b33d351c530d51c9d00b366b2983
DaviSL <davisimoeslucas@gmail.com>
2024-10-06 21:58:21 -0300

    MergeArray

--- exercises/MargeArray.py
+++ exercises/MargeArray.py
@@ -6,5 +6,13 @@ class Solution:
         result = m + n -1 
 
         while nIndex >= 0:
+            if mIndex >=0 and nums1[mIndex] > nums2[nIndex]:
+                nums1[result] = nums1[mIndex]
+                mIndex -= 1
+            else:
+                nums1[result] = nums2[nIndex]
+                nIndex -= 1
             
+            result -= 1
+
        
