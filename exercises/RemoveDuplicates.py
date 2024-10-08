77d3c1a8ef78fb2f8fdefd589380bd84895695b4
DaviSL <davisimoeslucas@gmail.com>
2024-10-07 23:03:41 -0300

    new version

--- exercises/RemoveDuplicates.py
+++ exercises/RemoveDuplicates.py
@@ -1,10 +1,10 @@
 class Solution:
     def removeDuplicates(self, nums: List[int]) -> int:
 
-        j = 1 # inicia a contagem em um pois vamos comparar sempre com o elemento anterior 
+        j = 1 # inicia a contagem em 1 pois para ser igual a alguém é necessário existir algum elemento no i = 0
 
-        for i in range(1, len(nums)): 
-            if nums[i] != nums[i-1]:
+        for i in range(1, len(nums)): # contador inicia em 1
+            if nums[i] != nums[i-1]: #verifica se o i é igual ao anterior 
                 nums[j] = nums[i]
                 j += 1
 
