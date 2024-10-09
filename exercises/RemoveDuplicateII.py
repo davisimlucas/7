4095bed68590c2f1874c2d7bdbc1a668bb5a7ec2
DaviSL <davisimoeslucas@gmail.com>
2024-10-09 00:25:55 -0300

    AD

--- exercises/RemoveDuplicateII.py
+++ exercises/RemoveDuplicateII.py
@@ -3,12 +3,12 @@ class Solution:
 
         #para casos de array curta de 2 elementos 
         if len(nums)<= 2:
-            return len(nums)
+            return len(nums) # retornar a própria array pois terá somente dois elementos mesmo que iguais
         
-        k =2 
-        for i in range(1, len(nums)):
+        k =2 #comparação deve iniciar no terceiro elemento, pois se estiver igual, seria a segunda repetição
+        for i in range(2, len(nums)):
             if nums[i] != nums[k-2]:
                 nums[k] = nums[i]
-                k =+ 1
+                k += 1
 
         return k
