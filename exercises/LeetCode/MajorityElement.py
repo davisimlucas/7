e509f266b27140943308b0d12d2a5712ffa26006
DaviSL <davisimoeslucas@gmail.com>
2024-10-12 15:59:09 -0300

    comments

--- exercises/LeetCode/MajorityElement.py
+++ exercises/LeetCode/MajorityElement.py
@@ -1,14 +1,17 @@
 class Solution:
     def majorityElement(self, nums: list[int])-> int:
-        
+        #dictionary utilizado para contagem de cada elemento em nums
         hash = {}
+        #res armazena o elemento majoritário e majoritary a contagem dele em nusm
         res = majority = 0 
 
         for n in nums:
+            #1+hash.get() add 1 ao valor atual ou inicializa caso n tenha aparecido pela 1x  
             hash[n] = 1 + hash.get(n, 0)
-            if hash[n] > majority:
+            #hash.get() obtem o valor associado a n no dict, se não apareceu ainda na lista, retorna 0
+            # se o elem já apareceu, ele retorna 1 unidade em hash[n]
+            if hash[n] > majority: 
+                # se a contagem n for maior que majority, res assume o valor n e majority assume o novo hash[n]
                 res = n
                 majority = hash[n]
         return res
-
-
