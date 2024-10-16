4fb9fe2b83a778c38d90a230374db51a34a6114d
DaviSL <davisimoeslucas@gmail.com>
2024-10-16 20:37:16 -0300

    comments

--- exercises/LeetCode/LongestCommonPrefix.py
+++ exercises/LeetCode/LongestCommonPrefix.py
@@ -1,13 +1,19 @@
 class Solution:
     def logestCommonPrefix(self, strs: List[str]) -> str:
-        prefix = strs[0] #primeira str 
-        prefixLen = len(prefix) #size da primeira str
+        #primeira str 
+        prefix = strs[0] 
+        #size da primeira str
+        prefixLen = len(prefix) 
 
-        for s in strs[1:]: #começar pela segunda str
+        #começar pela segunda str até a última 
+        for s in strs[1:]: 
+            # verificar se a primeira palavra é dif do resto em strs até o tamanho do prefix 
             while prefix != s[0:prefixLen]: 
+                # enq for True, diminuir o tamanho palavra que tiver comparando
                 prefixLen -= 1
+                # se terminar a palavre prefix ==> prefixLen = 0, retornar ""
                 if prefixLen == 0:
                     return ""
-                
+                #prefix assume o tamanho novo e volta no looping 
                 prefix = prefix[0:prefixLen]
         return prefix
